f1de0142bb0262d8d0274615ccd7187c63152ed3
Hamad Numan <hamadnuman.dev@gmail.com>
2022-05-31 00:51:23 +0300

    displaying tasks

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
 # todo-app-django
 
 1. install the python virtual environment
-    
-    python -m venv venv
 
+   python -m venv venv
 
-2. install the requirements
+2. activate the virtual environment
+3. install the requirements
 
-    pip install -r requirements.txt 
-
-
-    
+   pip install -r requirements.txt
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,13 @@
+from multiprocessing import context
 from django.shortcuts import render
-from django.http import HttpResponse 
-
+from django.http import HttpResponse
+from django.template import loader 
+from .models import Tasks
 def index(request):
-    return HttpResponse('Hello World!')
-
+    tasks = Tasks.objects.all().values()
+    template = loader.get_template('index.html')
+    context = {
+        'mytasks' : tasks
+    }
+    return HttpResponse(template.render(context, request))
 # Create your views here.
