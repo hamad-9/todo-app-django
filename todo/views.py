12e71c75e6e082d4bb0b0d18570652c8131745cd
Hamad Numan <hamadnuman.dev@gmail.com>
2022-05-31 01:29:45 +0300

    update and delete

--- todo/views.py
+++ todo/views.py
@@ -26,3 +26,25 @@ def addrecord(request):
   task = Tasks(description=x,)
   task.save()
   return HttpResponseRedirect(reverse('index'))
+
+
+def delete(request, id):
+    task = Tasks.objects.get(id=id)
+    task.delete()
+    return HttpResponseRedirect(reverse('index'))
+
+def update(request, id):
+    mytask = Tasks.objects.get(id=id)
+    template = loader.get_template('update.html')
+    context = {
+    'mytask': mytask,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def updaterecord(request, id):
+    description = request.POST['description']
+    task = Tasks.objects.get(id=id)
+    task.description = description
+    task.save()
+    return HttpResponseRedirect(reverse('index'))
